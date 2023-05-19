8a9d66963a969f453cb4b2bb52f88febdb5caa96
Simon Chanzu <chanzusimon6@gmail.com>
2023-05-19 11:40:51 +0300

    upload of routing assignment

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,26 @@
 import "./notfound.css"
+import { Link } from "react-router-dom"
+
 
 function NotFound() {
   return (
-    <div >
+    <div class="error" >
+        <div>
+        <h2>404 Not found </h2>
 
-    <h2>404 Not found</h2></div>
+        </div>
+        <div>
+        <h4>Hit the link below to redirect to the home page</h4>
+        </div>
+        <div className="button">
+      
+          <button><Link  to='./' > Home</Link></button>
+        </div>
+    
+    <br />    
+   
+
+    </div>
   )
 }
 
